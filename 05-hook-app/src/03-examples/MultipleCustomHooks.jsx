81b3ee02c2c4fe443f02d23915d19e7da981c9ac
Ignacio Ainol <iggnaxios@gmail.com>
2022-05-03 22:00:13 -0400

    separating components loading and quote

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,5 +1,6 @@
-import { useCounter } from "../hooks/useCounter";
-import { useFetch } from "../hooks/useFetch"
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote } from "./components/LoadingQuote";
+import { Quote } from "./components/Quote";
 
 export const MultipleCustomHooks = () => {
 
@@ -15,14 +16,9 @@ export const MultipleCustomHooks = () => {
       {
         isLoading
           ? (
-            <div className="alert alert-info text-center">
-              Loading ...
-            </div>
+            <LoadingQuote />
           ) : (
-            <blockquote className="blockquote text-end">
-              <p className="mb-1">{quote}</p>
-              <footer className="blockquote-footer">{author}</footer>
-            </blockquote>
+            <Quote author={author} quote={quote} />
           )
       }
 
