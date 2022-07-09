9adf7ec35175c8d0f075807597add7a51c8faad3
Ignacio Ainol <iggnaxios@gmail.com>
2022-07-09 16:17:13 -0400

    toEqual jest

---
+++ 03-counter-app-vite/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,30 @@
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
+
+describe('Test en 05-funciones', () => {
+
+  test('getUser debe retornar un objeto', () => {
+    const testUser = {
+      uid: 'ABC123',
+      username: 'El_Papi1502'
+    };
+
+    const user = getUser();
+    expect(testUser).toEqual(user);
+  })
+
+  test('getUsuario debe retornar un objeto', () => {
+    const name = 'Ignacio';
+
+    const userActive = {
+      uid: 'ABC567',
+      username: name
+    }
+
+    const user = getUsuarioActivo(name);
+
+    expect(user).toEqual(userActive);
+
+
+  })
+
+})
