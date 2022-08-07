e664c44d9317f2fa6b95b941590a359e6e926537
Ignacio Ainol <iggnaxios@gmail.com>
2022-08-07 14:04:27 -0400

    custom hook to forms

--- 05-hook-app/src/02-useEffect/FormWithCustomHook.jsx
+++ 05-hook-app/src/02-useEffect/FormWithCustomHook.jsx
@@ -1,47 +1,19 @@
 import { useEffect, useState } from "react"
-import { Message } from "./Message";
+import { useForm } from "../hooks/useForm"
 
-export const SimpleForm = () => {
+export const FormWithCustomHook = () => {
 
-  const [formState, setFormState] = useState({
-    username: 'strider',
-    email: 'ignacio@google.com'
+  const { formState, onResetForm, onInputChange, username, email, password } = useForm({
+    username: '',
+    email: '',
+    password: ''
   });
 
-  const { username, email } = formState;
-
-  const onInputChange = ({ target }) => {
-    const { name, value } = target;
-    setFormState({
-      ...formState,
-      [name]: value
-    })
-  }
-
-  useEffect(() => {
-    // console.log('useEffect called');
-  }, [])
-
-  useEffect(() => {
-    // console.log('formState changed');
-  }, [formState])
-
-  useEffect(() => {
-    // console.log('email changed');
-  }, [email])
-
-  // useEffect(() => {
-  //   first
-
-  //   return () => {
-  //     second
-  //   }
-  // }, [third])
-
+  // const { username, email, password } = formState;
 
   return (
     <>
-      <h1>Formulario Basic</h1>
+      <h1>Form With Custom Hook</h1>
       <hr />
 
       <input
@@ -62,11 +34,17 @@ export const SimpleForm = () => {
         onChange={onInputChange}
       />
 
-      {
-        username === 'strider2' && (
-          <Message />
-        )
-      }
+      <input
+        type="password"
+        className="form-control mt-2"
+        placeholder="******"
+        name="password"
+        value={password}
+        onChange={onInputChange}
+      />
+
+      <button onClick={onResetForm} className="btn btn-primary mt-2">Reset</button>
+
     </>
   )
 }
